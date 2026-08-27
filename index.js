6f3ca6c590d38a15de0b601b3f7ecde581c9e5f6
Swathy1999 <swathylatika@gmail.com>
2026-08-27 21:28:05 +0530

    server corrections

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const jsonServer = require('json-server')
 const server = jsonServer.create()
-const router = jsonServer.router('db.json')
+const router = jsonServer.router('data.json')
 const middleware = jsonServer.defaults()
 const PORT = 3000
 server.use(middleware)
